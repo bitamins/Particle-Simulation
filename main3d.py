5294b19e728a1a32e8ce1f4ee199b44dde7b4ddf
bitamins <msbridges99@gmail.com>
2020-04-08 18:56:15 -0700

    3 body simulation changes

--- main3d.py
+++ main3d.py
@@ -8,10 +8,11 @@ import pygame
 import random
 import math
 import numpy as np
+import time
 
 
-WIDTH = 1080
-HEIGHT = 720
+WIDTH = 500
+HEIGHT = 500
 DEPTH = 500
 
 BG = {'white':(255,255,255),
@@ -20,32 +21,77 @@ BG = {'white':(255,255,255),
       }
         
 
-def generate_random_particles(n_particles=5,max_height=HEIGHT,max_width=WIDTH,max_depth=DEPTH,max_velocity=5.,min_velocity=.1,max_mass=10.,min_mass=1.,max_size=10.,min_size=2.):     
-    dist = np.random.uniform(size=(n_particles,)) 
+def generate_random_particles(n_particles=5,max_height=HEIGHT,max_width=WIDTH,max_depth=DEPTH,max_velocity=5.,min_velocity=.1,max_mass=10.,min_mass=1.,max_size=10.,min_size=4.):     
+    dist = np.random.uniform(size=(n_particles,))
+    
     mass = (dist * (max_mass-min_mass)) + min_mass
-    size = (dist * (max_size-min_size)) + min_size 
+    radius = (dist * (max_size-min_size)) + min_size 
+        
+    AX = np.zeros((n_particles,))
+    AY = np.zeros((n_particles,))
+    AZ = np.zeros((n_particles,))
     
-    # VX = np.random.uniform(size=(n_particles,)) * max_velocity  * np.random.choice(np.array([1.,-1.]),n_particles)
-    # VY = np.random.normal(size=(n_particles,)) * max_velocity * np.random.choice(np.array([1.,-1.]),n_particles)
-    # VZ = np.random.uniform(size=(n_particles,)) * max_velocity * -1.
-
-    VX = np.random.normal(loc=0.0,scale=3.0,size=(n_particles,)) * max_velocity  * np.random.choice(np.array([1.,-1.]),n_particles)
-    VY = np.random.normal(size=(n_particles,)) * max_velocity * np.random.choice(np.array([1.,-1.]),n_particles)
-    VZ = np.random.uniform(size=(n_particles,)) * max_velocity * -1.
+    VX = np.ones((n_particles,)) / 2
+    VY = np.ones((n_particles,)) / 2
+    VZ = np.ones((n_particles,)) / 2
     
-    X = np.ones((n_particles,)) / 2. * max_width
-    Y = np.ones((n_particles,)) / 2. * max_height
-    Z = np.ones((n_particles,)) * max_depth
+    X = np.random.uniform(size=(n_particles,)) * max_width
+    Y = np.random.uniform(size=(n_particles,)) * max_height
+    Z = np.random.uniform(size=(n_particles,)) * max_depth
+    
             
-    red = np.random.randint(200,245,n_particles)
-    green = np.random.randint(200,245,n_particles)
-    blue = np.random.randint(200,245,n_particles)
+    red = np.random.randint(100,245,n_particles)
+    green = np.random.randint(100,245,n_particles)
+    blue = np.random.randint(100,245,n_particles)
     
-    particles = np.array([X,Y,Z,VX,VY,VZ,red,green,blue,mass,size]).T
+    particles = np.array([X,Y,Z,VX,VY,VZ,AX,AY,AZ,red,green,blue,mass,radius]).T
 
     return particles
        
-       
+
+def check_forces(particles,x_idx,y_idx,z_idx,ax_idx,ay_idx,az_idx,mass_idx):
+    G = 6.67e-5
+    # G = 1.
+    for i in range(particles.shape[0]):
+        F = np.array([0.,0.,0.])
+        for j in range(i+1,particles.shape[0]):
+            F[0] += G * particles[i,mass_idx] * particles[j,mass_idx] / (particles[i,x_idx] - particles[j,x_idx])**2 * np.sign((particles[j,x_idx] - particles[i,x_idx]))
+            F[1] += G * particles[i,mass_idx] * particles[j,mass_idx] / (particles[i,y_idx] - particles[j,y_idx])**2 * np.sign((particles[j,y_idx] - particles[i,y_idx]))
+            F[2] += G * particles[i,mass_idx] * particles[j,mass_idx] / (particles[i,z_idx] - particles[j,z_idx])**2 * np.sign((particles[j,z_idx] - particles[i,z_idx]))
+        if np.sum(F):
+            particles[i,ax_idx] = F[0] / particles[i,mass_idx]
+            particles[i,ay_idx] = F[1] / particles[i,mass_idx]
+            particles[i,az_idx] = F[2] / particles[i,mass_idx]
+            print(F)
+            
+    return particles
+
+def move_particles_wrap_force(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,ax_idx,ay_idx,az_idx,size_idx,height,width,depth,max_velocity,min_velocity,max_mass,min_mass,max_size,min_size,step=1.):
+    # move particles by adding velocity
+    particles[:,vx_idx] += particles[:,ax_idx] * step
+    particles[:,vy_idx] += particles[:,ay_idx] * step
+    # particles[:,vz_idx] += particles[:,az_idx] * step
+    
+    particles[:,x_idx] += particles[:,vx_idx] * step
+    particles[:,y_idx] += particles[:,vy_idx] * step
+    # particles[:,z_idx] += particles[:,vz_idx] * step
+    
+    # if offscreen
+    particles_idx = np.argwhere(np.logical_or.reduce((particles[:,x_idx]<=0.-particles[:,size_idx],
+                                                      particles[:,x_idx]>=width+particles[:,size_idx],
+                                                      particles[:,y_idx]<=0.-particles[:,size_idx],
+                                                      particles[:,y_idx]>=height+particles[:,size_idx],
+                                                    #   particles[:,z_idx]<=0.-particles[:,size_idx],
+                                                    #   particles[:,z_idx]>=depth+particles[:,size_idx],
+                                                      ))).T[0]
+    
+    
+    # particles[:,x_idx] %= width
+    # particles[:,y_idx] %= height
+    # particles[:,z_idx] %= depth
+        
+    return particles
+
 def move_particles_random(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,height,width,depth,max_velocity,min_velocity,max_mass,min_mass,max_size,min_size,step=1):
     # move particles by adding velocity
     particles[:,x_idx] += particles[:,vx_idx] * step
@@ -67,40 +113,106 @@ def move_particles_random(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_
         
     return particles
 
-def move_particles_wrap(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,height,width,depth):
-    # wrap particles around the screen
-    particles[:,x_idx] += particles[:,vx_idx]
-    particles[:,y_idx] += particles[:,vy_idx]
-    particles[:,z_idx] += particles[:,vz_idx]
-    
-    particles[:,x_idx] %= width
-    particles[:,y_idx] %= height
-    particles[:,z_idx] %= depth
+def inelastic_collision(particles,pairs,vx_idx,vy_idx,vz_idx,mass_idx):
+    for i,j in pairs:
+        # i,j = pair[0],pair[1]
         
+        m1m2 = (particles[i,mass_idx] + particles[j,mass_idx])
+        
+        particles[i,vx_idx] =   particles[i,vx_idx] * ((particles[i,mass_idx] - particles[j,mass_idx]) / m1m2) + \
+                                particles[j,vx_idx] * (2 * particles[j,mass_idx] / m1m2)
+                                
+        particles[j,vx_idx] =   particles[i,vx_idx] * (2 * particles[i,mass_idx] / m1m2) + \
+                                particles[j,vx_idx] * ((particles[j,mass_idx] - particles[i,mass_idx]) / m1m2)
+        
+        particles[i,vy_idx] =   particles[i,vy_idx] * ((particles[i,mass_idx] - particles[j,mass_idx]) / m1m2) + \
+                                particles[j,vy_idx] * (2 * particles[j,mass_idx] / m1m2)
+        
+        particles[j,vy_idx] =   particles[i,vy_idx] * (2 * particles[i,mass_idx] / m1m2) + \
+                                particles[j,vy_idx] * ((particles[j,mass_idx] - particles[i,mass_idx]) / m1m2)
+                                
+        particles[i,vz_idx] =   particles[i,vz_idx] * ((particles[i,mass_idx] - particles[j,mass_idx]) / m1m2) + \
+                                particles[j,vz_idx] * (2 * particles[j,mass_idx] / m1m2)
+        
+        particles[j,vz_idx] =   particles[i,vz_idx] * (2 * particles[i,mass_idx] / m1m2) + \
+                                particles[j,vz_idx] * ((particles[j,mass_idx] - particles[i,mass_idx]) / m1m2) 
+    
+    return particles
+
+
+def elastic_collision(particles,pairs,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,mass_idx):
+    for i,j in pairs:
+
+        particles[i,vx_idx] = particles[i,vx_idx] - (2 * particles[j,mass_idx] / (particles[i,mass_idx] + particles[j,mass_idx])) \
+                                                    * np.dot((particles[i,vx_idx] - particles[j,vx_idx]),(particles[i,x_idx] - particles[j,x_idx])) \
+                                                    / (np.abs(particles[i,x_idx] - particles[j,x_idx]) ** 2) * (particles[i,x_idx] - particles[j,x_idx])
+        
+        particles[j,vx_idx] = particles[j,vx_idx] - (2 * particles[i,mass_idx] / (particles[i,mass_idx] + particles[j,mass_idx])) \
+                                                    * np.dot((particles[j,vx_idx] - particles[i,vx_idx]),(particles[j,x_idx] - particles[i,x_idx])) \
+                                                    / (np.abs(particles[j,x_idx] - particles[i,x_idx]) ** 2) * (particles[j,x_idx] - particles[i,x_idx])
+                                                    
+        particles[i,vy_idx] = particles[i,vy_idx] - (2 * particles[j,mass_idx] / (particles[i,mass_idx] + particles[j,mass_idx])) \
+                                                    * np.dot((particles[i,vy_idx] - particles[j,vy_idx]),(particles[i,y_idx] - particles[j,y_idx])) \
+                                                    / (np.abs(particles[i,y_idx] - particles[j,y_idx]) ** 2) * (particles[i,y_idx] - particles[j,y_idx])
+        
+        particles[j,vy_idx] = particles[j,vy_idx] - (2 * particles[i,mass_idx] / (particles[i,mass_idx] + particles[j,mass_idx])) \
+                                                    * np.dot((particles[j,vy_idx] - particles[i,vy_idx]),(particles[j,y_idx] - particles[i,y_idx])) \
+                                                    / (np.abs(particles[j,y_idx] - particles[i,y_idx]) ** 2) * (particles[j,y_idx] - particles[i,y_idx])
+
+    
+    return particles                         
+
+def check_walls(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,height,width,depth):
+    left_right = np.argwhere(np.logical_or.reduce((particles[:,x_idx]<0.+particles[:,size_idx],particles[:,x_idx]>width-particles[:,size_idx])))
+    top_bottom = np.argwhere(np.logical_or.reduce((particles[:,y_idx]<0.+particles[:,size_idx],particles[:,y_idx]>height-particles[:,size_idx])))
+    front_back = np.argwhere(np.logical_or.reduce((particles[:,z_idx]<0.+particles[:,size_idx],particles[:,z_idx]>depth-particles[:,size_idx])))
+
+    particles[left_right,vx_idx] *= -1.
+    particles[top_bottom,vy_idx] *= -1.
+    particles[front_back,vz_idx] *= -1.
+    
     return particles
 
 
 def check_collisions(particles,x_idx,y_idx,z_idx,size_idx):
-    # collisions = np.zeros((particles.shape[0],particles.shape[0]))
     pairs = []
     for i in range(particles.shape[0]):
         for j in range(i+1,particles.shape[0]):
-            dist = np.linalg.norm(np.array([particles[i,x_idx],particles[i,y_idx],particles[i,z_idx]]) - np.array([particles[j,x_idx],particles[j,y_idx],particles[j,z_idx]]),axis=-1)
+            # dist = np.linalg.norm( np.array([particles[i,x_idx],particles[i,y_idx],particles[i,z_idx]]) - np.array([particles[j,x_idx],particles[j,y_idx],particles[j,z_idx]]) )
+            dist = np.linalg.norm(np.array([particles[i,x_idx],particles[i,y_idx]]) - np.array([particles[j,x_idx],particles[j,y_idx]]))
             overlap = particles[i,size_idx] + particles[j,size_idx]
             if dist < overlap:
                 pairs.append([i,j])
     return np.array(pairs)
-            
-def draw_collisions(screen,particles,pairs,r_idx,g_idx,b_idx,x_idx,y_idx,size_idx):
-    # draw a line between two collided particles
-    color = (0,0,0)
-    thickness = 2 
+
+def merge_collisions(particles,pairs,x_idx,y_idx,z_idx,mass_idx,radius_idx):
+    new_particles = 0
     for pair in pairs:
         i,j = pair[0],pair[1]
-        pygame.draw.line(screen,color,(particles[i,x_idx],particles[i,y_idx]),(particles[j,x_idx],particles[j,y_idx]),thickness)
+        new_particle = np.mean(np.array([particles[i],particles[j]]),axis=0)
+            
+def draw_collisions(screen,particles,pairs,r_idx,g_idx,b_idx,x_idx,y_idx):
+    # draw a line between two collided particles
+    color = (255,255,255)
+    thickness = 2 
+    if pairs.shape[0]:
+        for pair in pairs:
+            i,j = pair[0],pair[1]
+            pygame.draw.line(screen,color,(particles[i,x_idx],particles[i,y_idx]),(particles[j,x_idx],particles[j,y_idx]),thickness)
+
+def draw_particles(screen,particles,r_idx,g_idx,b_idx,x_idx,y_idx,z_idx,size_idx,depth):
+    # draw particles with darkening gradient history tail
+    for particle in particles:
+        color = (int(particle[r_idx]),int(particle[g_idx]),int(particle[b_idx]))
+        pos = (int(particle[x_idx]),int(particle[y_idx]))
+        # size = int((particle[size_idx] * (1. - ((particle[z_idx])/(depth+particle[size_idx])))))
+        size = int(particle[size_idx])
+        if size < 0.:
+            size = 0
+        pygame.draw.circle(screen,color,pos,size,0)
 
 
-def draw_particles(screen,particles,r_idx,g_idx,b_idx,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,max_depth,trail_size=3,trail_stride=1,trail_gradient=0.5):
+def draw_particles_history(screen,particles,r_idx,g_idx,b_idx,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,max_depth,trail_size=3,trail_stride=1,trail_gradient=0.5):
     # draw particles with darkening gradient history tail
     for particle in particles:
         # compute color value ratios for history tail
@@ -137,13 +249,13 @@ def main():
     screen = pygame.display.set_mode([width, height])
     
     # hyperparameters, these can be changed in all sorts of fun ways.
-    n_particles = int(1e3)
+    n_particles = int(10)
     
     max_velocity=3.
     min_velocity=.5
     
-    max_mass=10.
-    min_mass=1.
+    max_mass=40.
+    min_mass=20.
     
     max_size=15.
     min_size=5.
@@ -155,13 +267,17 @@ def main():
     vx_idx = 3
     vy_idx = 4
     vz_idx = 5
-
-    r_idx = 6
-    g_idx = 7
-    b_idx = 8
     
-    mass_idx = 9
-    size_idx = 10
+    ax_idx = 6
+    ay_idx = 7
+    az_idx = 8
+    
+    r_idx = 9
+    g_idx = 10
+    b_idx = 11
+    
+    mass_idx = 12
+    size_idx = 13
     
     # history_size = 3
     
@@ -182,11 +298,26 @@ def main():
         screen.fill(BG['gray'])
 
         # draw the particles
-        draw_particles(screen,particles,r_idx,g_idx,b_idx,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,depth)
+        draw_particles(screen,particles,r_idx,g_idx,b_idx,x_idx,y_idx,z_idx,size_idx,depth)
+        
+        # identify collisions
+        pairs = check_collisions(particles,x_idx,y_idx,z_idx,size_idx)
+        
+        draw_collisions(screen,particles,pairs,r_idx,g_idx,b_idx,x_idx,y_idx)
+        
+        particles = elastic_collision(particles,pairs,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,mass_idx)
+        
+        particles = check_walls(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,height,width,depth)
+        
+        # calculate forces
+        particles = check_forces(particles,x_idx,y_idx,z_idx,ax_idx,ay_idx,az_idx,mass_idx)
             
         # move the particles
-        particles = move_particles_random(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,size_idx,height,width,depth,max_velocity,min_velocity,max_mass,min_mass,max_size,min_size)
-        # particles_history = np.concatenate((np.array([particles]),particles_history),axis=0)[:history_size]
+        particles = move_particles_wrap_force(particles,x_idx,y_idx,z_idx,vx_idx,vy_idx,vz_idx,ax_idx,ay_idx,az_idx,size_idx,height,width,depth,max_velocity,min_velocity,max_mass,min_mass,max_size,min_size)
+        
+        time.sleep(.01)
+        # print(particles[:,3:6])
+        # print(particles.shape)
         
         # Flip the display
         pygame.display.flip()
